8f0ece606bf4d3e002125b0a9235fda50a5cca4d
S3rv3r5p41n <47113566+XrPF@users.noreply.github.com>
2024-02-18 19:28:49 +0100

    Add files via upload

--- app/services/mongodb_service.py
+++ app/services/mongodb_service.py
@@ -1,7 +1,6 @@
 import os
 import time
 import math
-import psutil
 import random
 import logging
 import threading
@@ -15,58 +14,36 @@ logger = logging.getLogger(__name__)
 
 class MongoDBService:
     def __init__(self):
+        self.load_env_vars()
+        self.init_mongo_connections()
+        self.init_document_processing()
+
+    def load_env_vars(self):
         load_dotenv()
-        if os.getenv('TOTAL_MACHINES') is not None and os.getenv('MACHINE_ID') is not None:
-            self.total_machines = int(os.getenv('TOTAL_MACHINES'))
-            self.machine_id = int(os.getenv('MACHINE_ID'))
-        else:
-            self.total_machines = 1
-            self.machine_id = 1
-        # MongoDB Connection
-        if os.getenv('MONGO_CONNECTION_STRING_1') is None and os.getenv('MONGO_CONNECTION_STRING_2') is None:
-            uri1 = f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOSTS_1')}/?{os.getenv('MONGO_OPTS_1')}"
-            uri2 = f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOSTS_2')}/?{os.getenv('MONGO_OPTS_2')}"
-        else:
-            uri1 = os.getenv('MONGO_CONNECTION_STRING_1')
-            uri2 = os.getenv('MONGO_CONNECTION_STRING_2')
+        self.total_machines = int(os.getenv('TOTAL_MACHINES', 1))
+        self.machine_id = int(os.getenv('MACHINE_ID', 1))
+        self.max_workers = int(os.getenv('MAX_WORKERS', 1))
+        self.percentage = float(os.getenv('PERCENTAGE', 0.2))
+
+    def init_mongo_connections(self):
+        uri1 = os.getenv('MONGO_CONNECTION_STRING_1') or self.build_mongo_uri('MONGO_HOSTS_1', 'MONGO_OPTS_1')
+        uri2 = os.getenv('MONGO_CONNECTION_STRING_2') or self.build_mongo_uri('MONGO_HOSTS_2', 'MONGO_OPTS_2')
         self.syncSrc = MongoClient(uri1)
         self.syncDst = MongoClient(uri2)
-        if os.getenv('MONGO_CONNECTION_STRING_REPLICA_1') is None and os.getenv('MONGO_CONNECTION_STRING_REPLICA_2') is None:
-            self.replicate_uri1 = f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOSTS_1')}/?{os.getenv('MONGO_OPTS_REPLICA_1')}"
-            self.replicate_uri2 = f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv('MONGO_HOSTS_2')}/?{os.getenv('MONGO_OPTS_REPLICA_2')}"
-        else:
-            self.replicate_uri1 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_1')
-            self.replicate_uri2 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_2')
-        if os.getenv('DB_NAME') is not None and os.getenv('COLLECTION_NAME') is not None:
-            self.db_name = os.getenv('DB_NAME')
-            self.collection_name = os.getenv('COLLECTION_NAME')
-            self.coll_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc)
-            self.coll_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst)
-        # Memory Management
-        total_memory = psutil.virtual_memory().total / float(1024 ** 2)
-        base_memory = self.memory_usage_psutil()
-        available_memory = total_memory - base_memory
-        memory_per_worker = (5 / 100 ) * total_memory
-        if os.getenv('MAX_WORKERS') is not None:
-            self.max_workers = int(os.getenv('MAX_WORKERS'))
-        else:
-            self.max_workers = int(available_memory / memory_per_worker)
-        self.mem_threshold = 0.4 * total_memory
-        logger.info(f'Memory Management: Total {total_memory} MB || Base {base_memory} MB || Avail {available_memory}')
-        logger.info(f'Memory Management: Max workers {self.max_workers} || Mem threshold {self.mem_threshold} || Mem x worker {memory_per_worker}')
-        # Percentage of documents to be processed
-        if os.getenv('PERCENTAGE') is not None:
-            self.percentage = float(os.getenv('PERCENTAGE'))
-        else:
-            self.percentage = 0.2
+        
+        self.db_name = os.getenv('DB_NAME')
+        self.collection_name = os.getenv('COLLECTION_NAME')
+
+        self.replicate_uri1 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_1') or self.build_mongo_uri('MONGO_HOSTS_1', 'MONGO_OPTS_REPLICA_1')
+        self.replicate_uri2 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_2') or self.build_mongo_uri('MONGO_HOSTS_2', 'MONGO_OPTS_REPLICA_2')
+
+    def build_mongo_uri(self, hosts_env_var, opts_env_var):
+        return f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv(hosts_env_var)}/?{os.getenv(opts_env_var)}"
+
+    def init_document_processing(self):
         self.total_docs = 0
         self.processed_docs = 0
         self.processed_docs_lock = threading.Lock()
-
-    def memory_usage_psutil(self):
-        process = psutil.Process(os.getpid())
-        mem = process.memory_info().rss / float(2 ** 20)
-        return max(mem, 0)
     
     def get_collection(self, db_name, collection_name, client):
         db = client[db_name]
@@ -80,59 +57,76 @@ class MongoDBService:
             self.syncSrc_replicate.close()
             self.syncDst_replicate.close()
     
-    def process_batch(self, i, batch_size, batch_file, upsert_key=None):
-        logger.debug(f'[{threading.current_thread().name}] ({i}): Start batch')
-        coll_src = self.coll_src
-        coll_dest = self.coll_dst
+    def calculate_sleep_time(self):
+        base_sleep_time = min(self.max_workers, 60)
+        return random.uniform((base_sleep_time / 2) / self.total_machines, base_sleep_time)
+    
+    def fetch_documents(self, i, batch_size, session):
+        return self.coll_src.find(session=session, no_cursor_timeout=True).sort('_id', 1).skip(i).limit(batch_size)
+    
+    def build_operations(self, i, cursor, upsert_key):
         operations = []
         num_ids = 0
-        base_sleep_time = min(self.max_workers, 60)
-        sleep_time = random.uniform((base_sleep_time / 2) / self.total_machines, base_sleep_time)
+        for doc in cursor:
+            num_ids += 1
+            update_key = {'_id': doc['_id']}
+            if upsert_key is not None:
+                update_key[upsert_key] = doc[upsert_key]
+            operations.append(UpdateOne(update_key, {'$set': doc}, upsert=True))
+            logger.debug(f'[{threading.current_thread().name}] ({i}): Upsert document with _id: {doc["_id"]}')
+        return operations, num_ids
+    
+    def write_documents(self, i, operations, batch_file, num_ids):
+        if operations:
+            try:
+                self.coll_dst.bulk_write(operations)
+                with open(batch_file, 'a') as f:
+                    f.write(f'{i}\n')
+                with self.processed_docs_lock:
+                    self.processed_docs += num_ids
+            except Exception as e:
+                logger.error(f'[{threading.current_thread().name}] ({i}): ERROR in bulk_write: {e}')
+                raise
+
+    def log_and_sleep(self, i, operations, num_ids, read_time, write_time, sleep_time):
+        logger.info(f'[{threading.current_thread().name}] ({i}): Fetched {num_ids} documents in {round(read_time, 3)} seconds.')
+        logger.info(f'[{threading.current_thread().name}] ({i}): Written {num_ids} documents in {round(write_time, 3)} seconds.')
+        progress, _ = self.sync_status_progress()
+        logger.info(f'[{threading.current_thread().name}] ({i}): Progress {progress} %')
+        if operations and write_time < read_time:
+            if write_time * 2 < read_time:
+                 read_sleep_time = random.uniform(read_time + sleep_time, read_time * 2 + sleep_time)
+            else:
+                read_sleep_time = random.uniform(read_time, read_time * 2)
+            logger.warn(f"[{threading.current_thread().name}] ({i}): Read threshold exceeded, let's take a break for {read_sleep_time} seconds...")
+            time.sleep(read_sleep_time)
+    
+    def process_batch(self, i, batch_size, batch_file, upsert_key=None):
+        logger.debug(f'[{threading.current_thread().name}] ({i}): Start batch')
+        sleep_time = self.calculate_sleep_time()
         logger.debug(f'[{threading.current_thread().name}] ({i}): Sleeping for {round(sleep_time, 1)} seconds...')
         time.sleep(sleep_time)
-        start_time = time.time()
 
         with self.syncSrc.start_session() as session:
-            cursor = coll_src.find(session=session, no_cursor_timeout=True).sort('_id', 1).skip(i).limit(batch_size)        
+            cursor = None
             try:
-                for doc in cursor:
-                    num_ids += 1
-                    update_key = {'_id': doc['_id']}
-                    if upsert_key is not None:
-                        update_key[upsert_key] = doc[upsert_key]
-                    operations.append(UpdateOne(update_key, {'$set': doc}, upsert=True))
-                    logger.debug(f'[{threading.current_thread().name}] ({i}): Upsert document with _id: {doc["_id"]}')
+                start_time = time.time()
+                cursor = self.fetch_documents(i, batch_size, session)
+                operations, num_ids = self.build_operations(i, cursor, upsert_key)
                 end_time = time.time()
                 read_time = round(end_time - start_time, 3)
-                logger.debug(f'[{threading.current_thread().name}] ({i}): Fetched {num_ids} documents in {read_time} seconds')
-
-                if operations:
-                    try:
-                        start_time = time.time()
-                        coll_dest.bulk_write(operations)
-                        with open(batch_file, 'a') as f:
-                            f.write(f'{i}\n')
-                        with self.processed_docs_lock:
-                            self.processed_docs += num_ids
-                    except Exception as e:
-                        logger.error(f'[{threading.current_thread().name}] ({i}): ERROR in bulk_write: {e}')
-                        raise
-                    else:
-                        end_time = time.time()
-                        write_time = round(end_time - start_time, 3)
-                        logger.debug(f'[{threading.current_thread().name}] ({i}): Document upserts: {len(operations)} in {write_time} seconds')
-                else:
-                    end_time = time.time()
-                    logger.debug(f'[{threading.current_thread().name}] ({i}): Document upserts: 0 in {round(end_time - start_time, 3)} seconds')
+                start_time = time.time()
+                self.write_documents(i, operations, batch_file, num_ids)
+                end_time = time.time()
+                write_time = round(end_time - start_time, 3)
+                self.log_and_sleep(i, operations, num_ids, read_time, write_time, sleep_time)
+            except Exception as e:
+                logger.error(f'[{threading.current_thread().name}] ({i}): ERROR: {e}')
+                raise
             finally:
-                cursor.close()
+                if cursor:
+                    cursor.close()
                 session.end_session()
-                percent_diff = (abs(read_time - write_time) / min(read_time, write_time)) * 100
-                logger.info(f'[{threading.current_thread().name}] ({i}): Read time: {read_time} seconds || Write time: {write_time} seconds || Percent diff: {round(percent_diff, 2)}%')
-                if write_time < read_time:
-                    read_sleep_time = random.uniform(read_time, read_time * 2)
-                    logger.warn(f"[{threading.current_thread().name}] ({i}): Read threshold exceeded, let's take a break for {read_sleep_time} seconds...")
-                    time.sleep(read_sleep_time)
 
     def calculate_batch_size(self, total_docs):
         return math.ceil((int(total_docs * self.percentage) // 100) / self.max_workers)
@@ -165,19 +159,35 @@ class MongoDBService:
         if db_name is not None and collection_name is not None:
             self.db_name = db_name
             self.collection_name = collection_name
-            self.coll_src = self.get_collection(db_name, collection_name, self.syncSrc)
-            self.coll_dst = self.get_collection(db_name, collection_name, self.syncDst)
+            self.sync_collection(upsert_key)
+        elif db_name is None and collection_name is None:
+            if self.db_name is not None and self.collection_name is not None:
+                self.sync_collection(upsert_key)
+            elif self.db_name is not None and self.collection_name is None:
+                for collection_name in self.syncSrc[self.db_name].list_collection_names():
+                    self.collection_name = collection_name
+                    self.sync_collection(upsert_key)
+            else:
+                for db_name in self.syncSrc.list_database_names():
+                    if db_name not in ['config', 'local', 'admin']:
+                        for collection_name in self.syncSrc[db_name].list_collection_names():
+                            self.db_name = db_name
+                            self.collection_name = collection_name
+                            self.sync_collection(upsert_key)
+        elif db_name is not None and collection_name is None:
+            self.db_name = db_name
+            for collection_name in self.syncSrc[self.db_name].list_collection_names():
+                self.collection_name = collection_name
+                self.sync_collection(upsert_key)
 
+
+    def sync_collection(self, upsert_key=None):
         batch_file=f'/tmp/{self.db_name}_{self.collection_name}_batch.txt'
-
-        logger.info(f'Looking for documents in {self.db_name}.{self.collection_name}')
-
-        self.total_docs = self.syncSrc[self.db_name][self.collection_name].estimated_document_count()
-
-        logger.info(f'Sync started for database {self.db_name}: {self.total_docs} estimated total documents in collection {self.collection_name}')
-
-        delay = random.uniform(0, self.max_workers)
-        time.sleep(delay)
+        self.coll_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc)
+        self.coll_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst)
+        self.total_docs = self.coll_src.estimated_document_count()
+        logger.info(f'[{self.machine_id}] Sync started for database {self.db_name}: {self.total_docs} estimated total documents in collection {self.collection_name}')
+        logger.info(f'[{self.machine_id}] Syncing {self.db_name}.{self.collection_name} with {self.max_workers} workers')
 
         if self.machine_id == 1:
             watcher = threading.Thread(target=self.replicate_changes)
@@ -193,22 +203,22 @@ class MongoDBService:
 
         self.process_batches(batch_size, batch_file, start_batch, end_batch, upsert_key)
         self.close_connections()
+        
+        if os.path.exists(batch_file):
+            os.remove(batch_file)
 
         if self.machine_id == 1:
             watcher.join()
 
-        logger.info(f'Sync ended for {self.db_name}.{self.collection_name}')
-
-        if os.path.exists(batch_file):
-            os.remove(batch_file)
-
-        logger.info(f'Cleaned up {batch_file}. Closed connections to databases and exiting...')
+        logger.info(f'[{self.machine_id}] Sync ended for {self.db_name}.{self.collection_name}')
+        logger.info(f'[{self.machine_id}] Cleaned up {batch_file}. Closed connections to databases and exiting...')
         
         subprocess.run(["sudo", "systemctl", "restart", "replica-sync-api.service"])
 
 
     def sync_status_progress(self):
-        progress = round((self.processed_docs / self.total_docs) * 100, 2)
+        total_docs_per_machine = self.total_docs / self.total_machines
+        progress = round((self.processed_docs / total_docs_per_machine) * 100, 2)
         progress_bar = '#' * int(progress) + '-' * (100 - int(progress))
         return f'{progress}% [{progress_bar}]'
     
@@ -216,32 +226,32 @@ class MongoDBService:
         if db_name is not None and collection_name is not None:
             self.db_name = db_name
             self.collection_name = collection_name
-            self.coll_src = self.get_collection(db_name, collection_name, self.syncSrc_replicate)
-            self.coll_dst = self.get_collection(db_name, collection_name, self.syncDst_replicate)
         self.syncSrc_replicate = MongoClient(self.replicate_uri1)
         self.syncDst_replicate = MongoClient(self.replicate_uri2)
+        collection_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc_replicate)
+        collection_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst_replicate)
 
         logger.info(f'[Real-Time-Replication] Starting to replicate changes for {self.db_name}.{self.collection_name}')
         try:
-            with self.syncSrc_replicate[self.db_name][self.collection_name].watch() as stream:
+            with collection_src.watch() as stream:
                 for change in stream:
                     operation_type = change['operationType']
                     document_key = change['documentKey']
                     logger.debug(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Change detected: {operation_type} {document_key}')
                     if operation_type == 'insert':
                         full_document = change['fullDocument']
-                        self.syncDst_replicate[self.db_name][self.collection_name].insert_one(full_document)
+                        collection_dst.insert_one(full_document)
                         logger.info(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Inserted document: {document_key}')
                     elif operation_type == 'update':
                         update_description = change['updateDescription']
-                        self.syncDst_replicate[self.db_name][self.collection_name].update_one(document_key, update_description)
+                        collection_dst.update_one(document_key, update_description)
                         logger.info(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Updated document: {document_key}')
                     elif operation_type == 'delete':
-                        self.syncDst_replicate[self.db_name][self.collection_name].delete_one(document_key)
+                        collection_dst.delete_one(document_key)
                         logger.info(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Deleted document: {document_key}')
                     elif operation_type == 'replace':
                         full_document = change['fullDocument']
-                        self.syncDst_replicate[self.db_name][self.collection_name].replace_one(document_key, full_document)
+                        collection_dst.replace_one(document_key, full_document)
                         logger.info(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Replaced document: {document_key}')
         except Exception as e:
             logger.error(f'[Real-Time-Replication] ({self.db_name}.{self.collection_name}) Error in replicate_changes: {e}')
