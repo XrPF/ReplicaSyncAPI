07733b2eaa149358474b35bbae1d2e1acd67184e
S3rv3r5p41n <47113566+XrPF@users.noreply.github.com>
2024-02-19 14:53:50 +0100

    Add files via upload

--- app/services/mongodb_service.py
+++ app/services/mongodb_service.py
@@ -20,8 +20,10 @@ class MongoDBService:
 
     def load_env_vars(self):
         load_dotenv()
-        self.total_machines = int(os.getenv('TOTAL_MACHINES', 1))
-        self.machine_id = int(os.getenv('MACHINE_ID', 1))
+        VM_WORKER_LIST = os.getenv('VM_WORKER_LIST')
+        split_workers = VM_WORKER_LIST.split(',') if VM_WORKER_LIST else []
+        self.total_machines = len(split_workers)
+        self.machine_id = int(os.getenv('VM_WORKER_ID'))
         self.max_workers = int(os.getenv('MAX_WORKERS', 1))
         self.percentage = float(os.getenv('PERCENTAGE', 0.2))
 
@@ -30,12 +32,8 @@ class MongoDBService:
         uri2 = os.getenv('MONGO_CONNECTION_STRING_2') or self.build_mongo_uri('MONGO_HOSTS_2', 'MONGO_OPTS_2')
         self.syncSrc = MongoClient(uri1)
         self.syncDst = MongoClient(uri2)
-        
         self.db_name = os.getenv('DB_NAME')
         self.collection_name = os.getenv('COLLECTION_NAME')
-
-        self.replicate_uri1 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_1') or self.build_mongo_uri('MONGO_HOSTS_1', 'MONGO_OPTS_REPLICA_1')
-        self.replicate_uri2 = os.getenv('MONGO_CONNECTION_STRING_REPLICA_2') or self.build_mongo_uri('MONGO_HOSTS_2', 'MONGO_OPTS_REPLICA_2')
 
     def build_mongo_uri(self, hosts_env_var, opts_env_var):
         return f"mongodb://{os.getenv('MONGO_USER')}:{os.getenv('MONGO_PASSWORD')}@{os.getenv(hosts_env_var)}/?{os.getenv(opts_env_var)}"
@@ -53,13 +51,21 @@ class MongoDBService:
     def close_connections(self):
         self.syncSrc.close()
         self.syncDst.close()
-        if self.machine_id != 1:
-            self.syncSrc_replicate.close()
-            self.syncDst_replicate.close()
+    
+    def calculate_batch_size(self, total_docs):
+        return math.ceil((int(total_docs * self.percentage) // 100) / self.max_workers)
     
     def calculate_sleep_time(self):
         base_sleep_time = min(self.max_workers, 60)
         return random.uniform((base_sleep_time / 2) / self.total_machines, base_sleep_time)
+    
+    def get_processed_batches(self, batch_file):
+        manager = Manager()
+        processed_batches = manager.list()
+        if os.path.exists(batch_file):
+            with open(batch_file, 'r') as f:
+                processed_batches.extend([int(line.strip()) for line in f])
+        return processed_batches
     
     def fetch_documents(self, i, batch_size, session):
         return self.coll_src.find(session=session, no_cursor_timeout=True).sort('_id', 1).skip(i).limit(batch_size)
@@ -89,10 +95,8 @@ class MongoDBService:
                 raise
 
     def log_and_sleep(self, i, operations, num_ids, read_time, write_time, sleep_time):
-        logger.info(f'[{threading.current_thread().name}] ({i}): Fetched {num_ids} documents in {round(read_time, 3)} seconds.')
-        logger.info(f'[{threading.current_thread().name}] ({i}): Written {num_ids} documents in {round(write_time, 3)} seconds.')
         progress = self.sync_status_progress().split('%')[0]
-        logger.info(f'[{threading.current_thread().name}] ({i}): Progress {progress} %')
+        logger.info(f'[{threading.current_thread().name}] ({i}): Fetched {num_ids} docs in {round(read_time, 3)}s. Written {num_ids} docs in {round(write_time, 3)}s. Progress: {progress}%')
         if operations and write_time < read_time:
             if write_time * 2 < read_time:
                  read_sleep_time = random.uniform(read_time + sleep_time, read_time * 2 + sleep_time)
@@ -100,6 +104,12 @@ class MongoDBService:
                 read_sleep_time = random.uniform(read_time, read_time * 2)
             logger.warn(f"[{threading.current_thread().name}] ({i}): Read threshold exceeded, let's take a break for {read_sleep_time} seconds...")
             time.sleep(read_sleep_time)
+
+    def sync_status_progress(self):
+        total_docs_per_machine = self.total_docs / self.total_machines
+        progress = round((self.processed_docs / total_docs_per_machine) * 100, 2)
+        progress_bar = '#' * int(progress) + '-' * (100 - int(progress))
+        return f'{progress}% [{progress_bar}]'
     
     def process_batch(self, i, batch_size, batch_file, upsert_key=None):
         logger.debug(f'[{threading.current_thread().name}] ({i}): Start batch')
@@ -128,17 +138,6 @@ class MongoDBService:
                     cursor.close()
                 session.end_session()
 
-    def calculate_batch_size(self, total_docs):
-        return math.ceil((int(total_docs * self.percentage) // 100) / self.max_workers)
-    
-    def get_processed_batches(self, batch_file):
-        manager = Manager()
-        processed_batches = manager.list()
-        if os.path.exists(batch_file):
-            with open(batch_file, 'r') as f:
-                processed_batches.extend([int(line.strip()) for line in f])
-        return processed_batches
-
     def process_batches(self, batch_size, batch_file, start_batch, end_batch, upsert_key=None):
         parent_batches = math.ceil(self.total_docs / batch_size)
         logger.info(f'Processing batches {start_batch}-{end_batch} with batch size is {batch_size}')
@@ -155,81 +154,82 @@ class MongoDBService:
 
         logger.info(f'Processed up to batch {end_batch}')
 
-    def compare_and_update(self, db_name=None, collection_name=None, upsert_key=None):
+    def compare_and_update(self, db_name=None, collection_name=None):
+        collections_to_sync = []
         if db_name is not None and collection_name is not None:
             self.db_name = db_name
             self.collection_name = collection_name
-            self.sync_collection(upsert_key)
+            logger.info(f'[{self.machine_id}] Database {self.db_name} and collection {self.collection_name} received. Starting sync...')
+            collections_to_sync.append((self.db_name, self.collection_name))
         elif db_name is None and collection_name is None:
             if self.db_name is not None and self.collection_name is not None:
-                self.sync_collection(upsert_key)
+                logger.info(f'[{self.machine_id}] Database {self.db_name} and collection {self.collection_name} found on .env vars. Starting sync...')
+                collections_to_sync.append((self.db_name, self.collection_name))
             elif self.db_name is not None and self.collection_name is None:
+                logger.info(f'[{self.machine_id}] Database {self.db_name} found on .env vars. Looking for collections to sync...')
                 for collection_name in self.syncSrc[self.db_name].list_collection_names():
                     self.collection_name = collection_name
-                    self.sync_collection(upsert_key)
+                    logger.info(f'[{self.machine_id}] Added collection {self.db_name}.{self.collection_name}')
+                    collections_to_sync.append((self.db_name, self.collection_name))
             else:
+                logger.info(f'[{self.machine_id}] No database and collection received. Looking for databases and collections to sync...')
                 for db_name in self.syncSrc.list_database_names():
                     if db_name not in ['config', 'local', 'admin']:
+                        logger.info(f'[{self.machine_id}] Found database {db_name}. Looking for collections to sync...')
                         for collection_name in self.syncSrc[db_name].list_collection_names():
                             self.db_name = db_name
                             self.collection_name = collection_name
-                            self.sync_collection(upsert_key)
+                            logger.info(f'[{self.machine_id}] Added collection {self.db_name}.{self.collection_name}')
+                            collections_to_sync.append((self.db_name, self.collection_name))
         elif db_name is not None and collection_name is None:
             self.db_name = db_name
+            logger.info(f'[{self.machine_id}] Database {self.db_name} received. Looking for collections to sync...')
             for collection_name in self.syncSrc[self.db_name].list_collection_names():
                 self.collection_name = collection_name
-                self.sync_collection(upsert_key)
+                logger.info(f'[{self.machine_id}] Added collection {self.db_name}.{self.collection_name}')
+                collections_to_sync.append((self.db_name, self.collection_name))
+        return collections_to_sync
 
-
-    def sync_collection(self, upsert_key=None):
-        batch_file=f'/tmp/{self.db_name}_{self.collection_name}_batch.txt'
-        self.coll_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc)
-        self.coll_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst)
-        self.total_docs = self.coll_src.estimated_document_count()
-        logger.info(f'[{self.machine_id}] Sync started for database {self.db_name}: {self.total_docs} estimated total documents in collection {self.collection_name}')
-        logger.info(f'[{self.machine_id}] Syncing {self.db_name}.{self.collection_name} with {self.max_workers} workers')
-
-        if self.machine_id == 1:
-            watcher = threading.Thread(target=self.replicate_changes)
-            watcher.start()
-
-        batch_size = self.calculate_batch_size(self.total_docs)
-        parent_batches = math.ceil(self.total_docs / batch_size)
-        batches_per_machine = math.ceil(parent_batches / self.total_machines)
-        start_batch = (self.machine_id - 1) * batches_per_machine
-        end_batch = min(start_batch + batches_per_machine, parent_batches)
-
-        logger.info(f'[{self.machine_id}] Batch size is {batch_size}. Parent batches: {parent_batches}. Batches per machine: {batches_per_machine}. Start batch: {start_batch}. End batch: {end_batch}')
-
-        self.process_batches(batch_size, batch_file, start_batch, end_batch, upsert_key)
-        self.close_connections()
-        
-        if os.path.exists(batch_file):
-            os.remove(batch_file)
-
-        if self.machine_id == 1:
-            watcher.join()
-
-        logger.info(f'[{self.machine_id}] Sync ended for {self.db_name}.{self.collection_name}')
-        logger.info(f'[{self.machine_id}] Cleaned up {batch_file}. Closed connections to databases and exiting...')
-        
-        subprocess.run(["sudo", "systemctl", "restart", "replica-sync-api.service"])
-
-
-    def sync_status_progress(self):
-        total_docs_per_machine = self.total_docs / self.total_machines
-        progress = round((self.processed_docs / total_docs_per_machine) * 100, 2)
-        progress_bar = '#' * int(progress) + '-' * (100 - int(progress))
-        return f'{progress}% [{progress_bar}]'
-    
-    def replicate_changes(self, db_name=None, collection_name=None):
-        if db_name is not None and collection_name is not None:
+    def sync_collection(self, db_name=None, collection_name=None, upsert_key=None):
+        for db_name, collection_name in self.compare_and_update(db_name, collection_name):
             self.db_name = db_name
             self.collection_name = collection_name
-        self.syncSrc_replicate = MongoClient(self.replicate_uri1)
-        self.syncDst_replicate = MongoClient(self.replicate_uri2)
-        collection_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc_replicate)
-        collection_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst_replicate)
+            batch_file=f'/tmp/{self.db_name}_{self.collection_name}_batch.txt'
+            self.coll_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc)
+            self.coll_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst)
+            self.total_docs = self.coll_src.estimated_document_count()
+            logger.info(f'[{self.machine_id}] ({self.db_name}) Estimated docs: {self.total_docs} in collection {self.collection_name}')
+            batch_size = self.calculate_batch_size(self.total_docs)
+            parent_batches = math.ceil(self.total_docs / batch_size)
+            batches_per_machine = math.ceil(parent_batches / self.total_machines)
+            start_batch = (self.machine_id - 1) * batches_per_machine
+            end_batch = min(start_batch + batches_per_machine, parent_batches)
+            logger.info(f'[{self.machine_id}] Batch size is {batch_size}. Parent batches: {parent_batches}. Batches per machine: {batches_per_machine}. Start batch: {start_batch}. End batch: {end_batch}')
+            self.process_batches(batch_size, batch_file, start_batch, end_batch, upsert_key)
+            if os.path.exists(batch_file):
+                os.remove(batch_file)
+            logger.info(f'[{self.machine_id}] Sync ended for {self.db_name}.{self.collection_name}. Closed connections to databases and exiting...')
+        self.close_connections()
+    
+    def start_replication(self, db_name=None, collection_name=None):
+        self.executor = None
+        self.futures = []
+        collections_to_replicate = self.compare_and_update(db_name, collection_name)
+        total_collections_to_replicate = len(collections_to_replicate)
+        self.executor = ThreadPoolExecutor(max_workers=total_collections_to_replicate)
+        self.futures = [self.executor.submit(self.replicate_changes, db_name, collection_name) for db_name, collection_name in collections_to_replicate]
+    
+    def stop_replication(self):
+        for future in self.futures:
+            future.cancel()
+        if self.executor:
+            self.executor.shutdown()
+
+    def replicate_changes(self, db_name, collection_name):
+        self.db_name = db_name
+        self.collection_name = collection_name
+        collection_src = self.get_collection(self.db_name, self.collection_name, self.syncSrc)
+        collection_dst = self.get_collection(self.db_name, self.collection_name, self.syncDst)
 
         logger.info(f'[Real-Time-Replication] Starting to replicate changes for {self.db_name}.{self.collection_name}')
         try:
