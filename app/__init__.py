6b6a47577d51dfc27cbbe33e5d84117ca7d6f057
S3rv3r5p41n <47113566+XrPF@users.noreply.github.com>
2024-03-04 10:04:16 +0100

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,1 @@
-from flask import Flask
-from .api.routes import api_blueprint
-import logging
-
-def create_app():
-    app = Flask(__name__)
-
-    handler = logging.FileHandler('/var/log/ReplicaSyncAPI/service.log')
-    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    handler.setFormatter(formatter)
-    handler.setLevel(logging.INFO)
-    app.logger.setLevel(logging.INFO)
-    app.logger.addHandler(handler)
-    app.register_blueprint(api_blueprint)
-
-    return app
+# This file is intentionally left blank
