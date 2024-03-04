3f152aa9b2e6633684c57a4a9f9f161752b1dbc9
S3rv3r5p41n <47113566+XrPF@users.noreply.github.com>
2024-03-04 10:05:02 +0100

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
-# This file is intentionally left blank
+from flask import Flask
+from .api.routes import api_blueprint
+import logging
+from logging.handlers import RotatingFileHandler
+
+def create_app():
+    app = Flask(__name__)
+
+    handler = RotatingFileHandler('/var/log/ReplicaSyncAPI/service.log', maxBytes=10000000, backupCount=5)
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.register_blueprint(api_blueprint)
+
+    return app
